5bbea8fbf6d847eb17646c198a453586409fb982
Rene Arias <rene_811@hotmail.com>
2026-04-20 14:33:30 -0500

    feat: add SEO_BASE_URL provider to server configuration for improved SEO handling

--- apps/web/src/app/app.config.server.ts
+++ apps/web/src/app/app.config.server.ts
@@ -3,8 +3,10 @@ import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 import { API_ENDPOINT_CONFIG } from '@arxis/api';
+import { SEO_BASE_URL, SEO_BASE_URL_STATE_KEY_NAME } from 'seo';
 
 export const API_URL_STATE_KEY = makeStateKey<string>('apiUrl');
+const SEO_BASE_URL_STATE_KEY = makeStateKey<string>(SEO_BASE_URL_STATE_KEY_NAME);
 
 const serverConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +17,15 @@ const serverConfig: ApplicationConfig = {
         const url = process.env['API_URL'] ?? '';
         transferState.set(API_URL_STATE_KEY, url);
         return { url };
+      },
+      deps: [TransferState],
+    },
+    {
+      provide: SEO_BASE_URL,
+      useFactory: (transferState: TransferState) => {
+        const url = process.env['BASE_URL'] ?? 'https://renearias.com';
+        transferState.set(SEO_BASE_URL_STATE_KEY, url);
+        return url;
       },
       deps: [TransferState],
     },
