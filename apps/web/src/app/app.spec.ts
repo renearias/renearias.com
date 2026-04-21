01855b6da84e064612a9bf8b05ede4b57fb05aca
Rene Arias <rene_811@hotmail.com>
2026-04-20 22:39:43 -0500

    feat: exclude api-e2e tests from CI pipeline for improved efficiency

--- apps/web/src/app/app.spec.ts
+++ apps/web/src/app/app.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 import { RouterModule } from '@angular/router';
+import { provideTranslateService } from '@ngx-translate/core';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App, RouterModule.forRoot([])],
+      providers: [provideTranslateService()],
     }).compileComponents();
   });
 
